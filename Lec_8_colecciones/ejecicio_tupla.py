c69bd1d682b06ad853aada97494e0cb6983631d9
Oscar Ramirez <oscar.ramirez@tigo.net.py>
2023-06-28 10:17:01 -0400

    doc: Agregamos ejercicio de tuplas

---
+++ Lec_8_colecciones/ejecicio_tupla.py
@@ -0,0 +1,9 @@
+#Crear una lista que solo incluya los números menores a 5
+tupla = (13,1,8,3,5,2,8,9)
+lista=[]
+
+for nro in tupla:
+        if nro <5:
+            lista.append(nro)
+print(lista)
+           
