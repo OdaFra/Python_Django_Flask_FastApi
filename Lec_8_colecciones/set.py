4c24f0e726b321e8fed9a58346e9eab5ef723022
Oscar Ramirez <oscar.ramirez@tigo.net.py>
2023-06-28 10:17:39 -0400

    doc: Agregamos archivo sobre el manejo de set.

---
+++ Lec_8_colecciones/set.py
@@ -0,0 +1,9 @@
+planetas ={'Marte','Venus','Jupiter', 'Tierra'}
+print(len(planetas))
+
+#Confirmar si un elemento esta presente
+print('Venus' in planetas)
+
+#Agregamos un nuevo elemento al set
+planetas.add('Tierra')
+print(planetas)
