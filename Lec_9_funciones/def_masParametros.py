01c20fdbc2a707d57f8a9581852bf5220722f185
Oscar Ramirez <osramirezf@gmail.com>
2023-07-18 18:48:04 -0400

    feat: Agregamos funciones de ejemplos.

---
+++ Lec_9_funciones/def_masParametros.py
@@ -0,0 +1,6 @@
+def listarNombres(*nombres):
+    for nombre in nombres:
+        print(nombre)
+
+listarNombres('Oscar','David','Naho','Vale')
+    
---
+++ Lec_9_funciones/funciones_doc.py
@@ -0,0 +1,8 @@
+def funcion(nombre, apellido):
+    print('saludos desde mi funcion')
+    print(f'Nombre: {nombre} ,Apellido: {apellido}')
+    
+
+funcion('Oscar', 'Ramirez')
+
+#Pasar argumentos a una funcion
