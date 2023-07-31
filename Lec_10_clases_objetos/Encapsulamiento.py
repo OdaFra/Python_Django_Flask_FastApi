9d3198239cbf517b3007909ee024a3fcae80af0d
Oscar Ramirez <osramirezf@gmail.com>
2023-07-31 19:37:58 -0400

    refactor: Aplicamos refactor para agregar privacidad a los atributos para que los mismos sean accedidos por getter y setter .

--- Lec_10_clases_objetos/Encapsulamiento.py
+++ Lec_10_clases_objetos/Encapsulamiento.py
@@ -10,25 +10,40 @@ class Persona:
         # Atributos
         # Encapsulamiento (atributo privado)
         self._nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
+        self._apellido = apellido
+        self._edad = edad
 
 #Manejo de GETTER y SETTER
     # Metodo Get
     @property
     def getNombre(self):
-        print("Llamando metodo get..")
         return self._nombre
 
     # Metodo setter
+        #Si se llegase a comentar esta seccion corresponde a una variable de solo lectura.
     @getNombre.setter
     def setNombre(self, nombre):
-        print("Llamando metodo set..")
         self._nombre = nombre
+
+    @property
+    def getApellido(self):
+        return self._apellido
+    
+    @getApellido.setter
+    def setApellido(self, apellido):
+        self._apellido=apellido
+
+    @property
+    def getEdad(self):
+        return self._edad
+    
+    @getEdad.setter
+    def setEdad(self, edad):
+        self._edad = edad
 
     # Creacion de metodo
     def mostrarDetalle(self):
-        print(f"Persona:{self._nombre}, {self.apellido}, {self.edad}")
+        print(f"Persona:{self._nombre}, {self._apellido}, {self._edad}")
 
 
 # Nuevo Objecto
@@ -36,5 +51,8 @@ person = Persona("Naho", "Ramirez", 7)
 # En python es posible aplicar esto pero desde el otros lenguajes no!
 # person._nombre='Vale'
 
-person.setNombre='Oscar'
-print(person.getNombre)
+if __name__ =='__main__':
+    person.setNombre='Oscar'
+    person.setApellido='Franco'
+    person.setEdad=32
+    person.mostrarDetalle()
