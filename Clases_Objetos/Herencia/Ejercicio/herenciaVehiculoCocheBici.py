05b37d2f0d8f2c5ca8a2e96a37859b0a02e2cac4
Oscar Ramirez <osramirezf@gmail.com>
2023-08-04 08:35:41 -0400

    feat: Testing/Ejericicio para el manejo de herencia desde una clase padre a varios clases hijas.

---
+++ Clases_Objetos/Herencia/Ejercicio/herenciaVehiculoCocheBici.py
@@ -0,0 +1,27 @@
+class Vehiculo:
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+    
+    def __str__(self):
+        return f'El vehiculo tiene el color: {self.color}, ruedas: {self.ruedas}'
+
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+        
+    def __str__(self):
+        return f'{super().__str__()}, la velocidad es: {self.velocidad}'
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo= tipo
+    
+    def __str__(self):
+        return f'{super().__str__()}, el tipo es: {self.tipo}'
+    
+
+vehiculo = Vehiculo('Azul', '4')
+print(vehiculo)
