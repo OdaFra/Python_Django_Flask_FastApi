7071e61a467412d8c49583f4c9c0bed62a7de08e
Oscar Ramirez <osramirezf@gmail.com>
2024-02-28 09:17:26 -0300

    feat: Prueba de import de la clase cuadrado para comprobar metodo.

--- 12-Herencia/Herencia Multiples/test_figuraGeometrica.py
+++ 12-Herencia/Herencia Multiples/test_figuraGeometrica.py
@@ -5,3 +5,7 @@ print(cuadrado1.alto)
 print(cuadrado1.ancho)
 print(cuadrado1.color)
 print(cuadrado1.calcular_area())
+
+#METODO MRO Method Resolution Order - Permiter conocer el orden en llamada a las clases heredadas
+
+print(Cuadrado.mro())
