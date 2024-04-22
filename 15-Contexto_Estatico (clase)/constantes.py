3f4d8d5dfd50e70bbfc612435ca2c827b4c8ac79
Oscar Ramirez <osramirezf@gmail.com>
2024-04-22 15:33:46 -0400

    feature: Manejo de constantes e implementacion de imports

---
+++ 15-Contexto_Estatico (clase)/constantes.py
@@ -0,0 +1,5 @@
+#Por convencion las constantes deben ir en mayusculas.
+Text_Constante = 'Aprendiendo Python'
+
+class Matematica:
+    PI = 3.1416
---
+++ 15-Contexto_Estatico (clase)/test.py
@@ -0,0 +1,11 @@
+from constantes import Text_Constante
+from constantes import Matematica as mate
+print(Text_Constante)
+print(mate.PI)
+
+
+
+#En python una constante puede ser modificada, sin embargo no se debe realizar el cambio por buenas practicas
+# Text_Constante = 'Aprendiendo golang'
+# print(Text_Constante)
+
