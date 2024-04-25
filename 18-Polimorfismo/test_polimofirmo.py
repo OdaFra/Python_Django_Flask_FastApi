15ee1e6d0a4f651cad822802898a207eb0476b82
Oscar Ramirez <osramirezf@gmail.com>
2024-04-25 11:01:01 -0400

    feature: Ejemplo de poliformismos.

---
+++ 18-Polimorfismo/test_polimofirmo.py
@@ -0,0 +1,16 @@
+from Empleado import Empleado
+from Gerente import Gerente
+
+def imprimir_detalles(objeto):
+    # print(objeto)
+    print(type(objeto))
+    print(objeto.mostrar_detalles())
+    if isinstance(objeto, Gerente):
+        print(objeto.departamento)
+
+empleado = Empleado('Juan', 2200)
+imprimir_detalles(empleado)
+
+gerente = Gerente('Alberto', 3000, 'Mecanica')
+imprimir_detalles(gerente)
+
