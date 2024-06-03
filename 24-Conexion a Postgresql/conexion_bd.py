9c82baae46c667fea3fd153d4ccb954fadac6370
Oscar Ramirez <osramirezf@gmail.com>
2024-06-03 12:32:57 -0400

    feat: Uso de with con sentencias especifica en conexion desde python postrgres

--- 24-Conexion a Postgresql/conexion_bd.py
+++ 24-Conexion a Postgresql/conexion_bd.py
@@ -8,18 +8,29 @@ conexion = pgdb.connect(
     database = 'postgres'
 )
 
+try:
 # print(conexion)
-with conexion:
-    #creamos el cursor hacia la db
-    with conexion.cursor() as cursor:
-        #generamos la sentencia a utilizar
-        sentencia = 'Select nombre, apellido FROM "Persona"'
-        #ejectuamos la sentencia por medio del cursor
-        cursor.execute(sentencia)
-        #obtenemos toda la informacion de la sentencia ejecutada en un lista (con tuplas)
-        res = cursor.fetchall()
-
-print(res)
-
-cursor.close()
-conexion.close()
+    with conexion:
+        #creamos el cursor hacia la db
+        with conexion.cursor() as cursor:
+            #generamos la sentencia a utilizar y obtener todas los registros
+            #sentencia = 'Select * FROM "Persona"'
+            #para obtener solo un registro
+            sentencia = 'Select * From "Persona" where id_persona in %s'
+            #id_persona= input('Proporcione el valor de id_persona: ')#2
+            # ids = ((1,3),)
+            entradas = input('Proporciona los ids a buscar separados por comas: ')
+            ids = (tuple(entradas.split(',')),)
+            #ejectuamos la sentencia por medio del cursor
+            cursor.execute(sentencia, ids)
+            #obtenemos toda la informacion de la sentencia ejecutada en un lista (con tuplas)
+            #res = cursor.fetchall()
+            #obtener solo un registro
+            res = cursor.fetchall()
+            
+            print(res)
+except Exception as e:
+    print (f'Ocurrio un error:{e}')
+finally:
+    #cursor.close()
+    conexion.close()
