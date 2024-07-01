4ccdd932361cafe279b651d38b8e48a8dd691848
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-07-01 19:47:47 -0400

    fix: Renombramos los archivos y agregamos el archivo de transaction

---
+++ 24-Conexion a Postgresql/transaction.py
@@ -0,0 +1,31 @@
+import psycopg2 as pgdb
+
+conexion = pgdb.connect(
+    user = 'postgres',
+    password = '123456',
+    host = 'localhost',
+    port = '5432',
+    database = 'postgres'
+)
+#Example for transaction
+try:
+    #Autocommit in false
+    conexion.autocommit = False
+    cursor = conexion.cursor()
+    #Insert
+    # sentencia = 'INSERT INTO "Persona"(nombre, apellido, email) values (%s,%s,%s)'        
+    # valores = ('Montse', 'Iglesias','miglesias@test.com')
+    # cursor.execute(sentencia, valores)
+    #Update
+    sentencia = 'Update "Persona" set nombre=%s, apellido=%s, email=%s where id_persona=%s'        
+    valores = ('Montse', 'Iglesias','miglesias@test.com', 8)
+    cursor.execute(sentencia, valores)
+    #set commit at the end
+    conexion.commit()
+    print('Termina la transaccion...!')
+except Exception as e:
+    conexion.rollback()
+    print (f'Ocurrio un error:{e}, se genera rollback..')
+finally:
+    #cursor.close()
+    conexion.close()
