292b5a0e7f5ac8eabcac92b15d83b968f751ef01
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-09 08:39:32 -0400

    fix: Inicio de profundizacion de Python (Sistema numerico)

---
+++ 31_Profundizando_numerico/sistemas_numeracion.py
@@ -0,0 +1,15 @@
+#Profundizando Sistema Binario
+#Decimal
+a=10
+#Binario
+a = 0b1010
+# print(f'a:{a}')
+#Octal
+a = 0o12
+# print(f'a:{a}')
+#Hexadecimal
+a = 0xA
+print(f'a:{a}')
+
+#convertir un tipo entero incluyendo la base
+a = int('23', 10)
