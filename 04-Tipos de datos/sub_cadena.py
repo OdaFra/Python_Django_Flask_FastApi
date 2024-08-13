a11ca20f7d36acc39fb3c2579cd7561eb0bfb994
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-13 09:48:01 -0400

    fix: Proceso de actualizacion de carpeta por actualizacion de curso

---
+++ 04-Tipos de datos/sub_cadena.py
@@ -0,0 +1,14 @@
+valor = 'Hola Mundo'
+sub_cadena = valor[5:-1]
+print(sub_cadena)
+
+#Metodo find
+varfind = valor.find('Mundo')
+print(varfind) # Devuelve el indice del str
+
+#Reemplazar str
+nuevo = valor.replace('Mundo','mundo a todos')
+print (nuevo)
+
+#Funcion Split
+
