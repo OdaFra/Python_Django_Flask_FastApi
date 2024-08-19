03f91933ce79e3afb2791569f6ce21d95d42e901
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-19 15:04:37 -0400

    fix: Ejericicio de str utilizando funciones upper, strip y obteniendo posiciones en base a una cadena de texto.

--- 04-Tipos de datos/funcion_input.py
+++ 04-Tipos de datos/funcion_input.py
@@ -16,7 +16,21 @@
 # print('Mi dia estuvo: ', data)
 
 #Ejercicio 2
-print('INFORMACION NECESARIA')
-titulo = input('Proporciona un titulo: ')
-autor = input('Proporciona un autor: ')
-print(titulo, ' fue escrito por el ', autor)
+# print('INFORMACION NECESARIA')
+# titulo = input('Proporciona un titulo: ')
+# autor = input('Proporciona un autor: ')
+# print(titulo, ' fue escrito por el ', autor)
+
+#Ejericio 3 - Sistema de generador de ID unico
+
+from random import randint
+
+nombre=input('Introduce tu Nombre: ')
+apellido=input('Introduce tu Apellido: ')
+anho=input('Introduce tu fecha de nacimiento: ')
+rango = randint(0,9999)
+id_unico = nombre.strip().upper()[0:2]+apellido.strip().upper()[0:2]+anho.strip()[2:]+rango[3:]
+print(f'El ID es: {id_unico}')
+
+
+
